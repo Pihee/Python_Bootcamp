6e0e111e6f0e29951c6404a7058bbddd1abb422e
Pihee <pierreedouardroux@gmail.com>
2024-12-09 10:22:54 +0100

    Leap Year or Not ?
    A program to tell if a given year is leap or not

---
+++ Pizza/main.py
@@ -0,0 +1,23 @@
+print("Thank you for choosing Python Pizza Deliveries!\n")
+size = input("What size pizza do you want? S, M, or L \n")
+add_pepperoni = input("Do you want pepperoni? Y or N \n")
+extra_cheese = input("Do you want extra cheese? Y or N \n")
+
+bill = 0
+if size == "L" :
+  bill = 25
+  if add_pepperoni == "Y" :
+    bill += 3
+elif size == "M" :
+  bill = 20
+  if add_pepperoni == "Y" :
+    bill += 3
+else :
+  bill = 15
+  if add_pepperoni == "Y" :
+    bill += 2
+
+if extra_cheese == "Y" :
+  bill += 1
+
+print(f"Your final bill is: ${bill}.")
